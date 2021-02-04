544566f031c8aeb0eec4f2bcbee4ea597cb58193
Bamba <bamba4>
2021-02-04 12:49:31 +0000

    Add static file

--- backend_dmdb/settings.py
+++ backend_dmdb/settings.py
@@ -133,10 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = {
-    os.path.join(BASE_DIR, 'static'),
-}
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 GRAPHENE = {
   "SCHEMA": "schema",
