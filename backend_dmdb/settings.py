26969efc1b3b1caddb647eaa2af7e8168f257425
Bamba <bamba4>
2021-02-05 20:08:34 +0000

    static file

--- backend_dmdb/settings.py
+++ backend_dmdb/settings.py
@@ -58,7 +58,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'backend_dmdb.urls'
 
