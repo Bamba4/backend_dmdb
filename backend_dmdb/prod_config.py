1d134e2854a71bd8c657a375073800f2d5a77f0a
Bamba <bamba4>
2021-02-06 16:14:33 +0000

    PUSH

--- backend_dmdb/prod_config.py
+++ backend_dmdb/prod_config.py
@@ -2,4 +2,5 @@ from .settings import *
 
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = "dj_database_url.config(conn_max_age=600, ssl_require=True)"
+DATABASES['default']['ENGINE']="mongoengine"
